b134610513444a4daa7161c81cdcf018b46354a1
Indzagi <Indzagi88@gmail.com>
2024-12-03 02:03:57 +0200

    dz2

---
+++ Python_lesson2/python_homework2_1.py
@@ -0,0 +1,14 @@
+number4 = int(input('Будь ласка, введіть 4-х значне число: '))
+
+firstnumb = number4 // 1000
+print(firstnumb)
+
+secondnumb = number4 // 100 % 10
+print(secondnumb)
+
+thirdnumb = number4 // 10 % 100 % 10
+print(thirdnumb)
+
+print(number4 % 10)
+
+
