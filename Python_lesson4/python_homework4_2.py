e9552d2a8577b330efe0a188d72badb7971bd02f
Indzagi <Indzagi88@gmail.com>
2024-12-11 19:09:05 +0200

    dz4

--- Python_lesson4/python_homework4_2.py
+++ Python_lesson4/python_homework4_2.py
@@ -17,6 +17,5 @@ if len(my_list) > 1:
 
 elif len(my_list) == 1:
     print(my_list[0] ** 2)
-
 else:
     print(0)
--- Python_lesson4/python_homework4_3.py
+++ Python_lesson4/python_homework4_3.py
@@ -1,4 +1,5 @@
 import random
+
 my_list = [random.randint(1, 10) for i in range(random.randrange(3, 11))]
 my_new_list = [my_list[0], my_list[2], my_list[-2]]
 print(my_list, my_new_list, sep=' => ')
