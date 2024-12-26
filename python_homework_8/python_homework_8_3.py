fb363266a3ea247b3b8625cfdcf5faa5597f5e5b
Indzagi <Indzagi88@gmail.com>
2024-12-26 15:18:01 +0200

    dz

--- python_homework_8/python_homework_8_3.py
+++ python_homework_8/python_homework_8_3.py
@@ -1,15 +1,12 @@
 def find_unique_value(some_list):
-    my_set = set()
-    #   for i in some_list:
-    #      my_firs_set.add(i)
-    #   print(my_firs_set)
-    return [num for num in some_list if num not in my_set and not my_set.add(num)]
+    for i in some_list:
+        if some_list.count(i) == 1:
+            return i
+    else:
+        print('Note Unique nombers')
 
 
-print(find_unique_value([1, 2, 1, 1]))
-print(find_unique_value([2, 3, 3, 3, 5, 5]))
-print(find_unique_value([5, 5, 5, 2, 2, 0.5]))
-# assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
-# assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
-# assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
 print("ОК")
