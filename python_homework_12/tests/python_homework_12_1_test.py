92775c4ebebf947fb1c405575eee2c24ce8f1763
Indzagi <Indzagi88@gmail.com>
2025-01-15 16:14:33 +0200

    dz

--- python_homework_12/tests/python_homework_12_1_test.py
+++ python_homework_12/tests/python_homework_12_1_test.py
@@ -2,6 +2,12 @@ import re
 
 
 def delete_html_tags(html_file, result_file='cleaned.txt'):
+    """
+    Function take text in 'draft.html' delete our html-tags
+    and save rezult in file 'cleaned.txt'
+    :param html_file: str in 'draft.html'
+    :param result_file: str in 'cleaned.txt'
+    """
     with open(html_file, 'r', encoding='utf-8') as file:
         html = file.read()
         html = re.sub(r'<[^>]+>', '', html)
